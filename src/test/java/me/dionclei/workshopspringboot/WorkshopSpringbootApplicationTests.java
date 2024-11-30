e670c8a5df3e0133b7d83eb30f3585a41740ac3b
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2024-11-30 09:52:22 -0300

    Initial commit

---
+++ src/test/java/me/dionclei/workshopspringboot/WorkshopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dionclei.workshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
