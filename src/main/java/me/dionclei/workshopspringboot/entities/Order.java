48f73a6454c979d71c5025f229e90c3f9a4726bf
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2024-12-14 19:59:43 -0300

    Subtotal and total methods

--- src/main/java/me/dionclei/workshopspringboot/entities/Order.java
+++ src/main/java/me/dionclei/workshopspringboot/entities/Order.java
@@ -97,6 +97,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Double getTotal() {
+		double sum = 0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(client, id, moment);
