94f901be3429694ce1bb84913a6872f7b559c8fe
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2024-12-15 21:27:50 -0300

    update and delete

--- src/main/java/me/dionclei/workshopspringboot/resources/UserResource.java
+++ src/main/java/me/dionclei/workshopspringboot/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,8 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import me.dionclei.workshopspringboot.entities.User;
 import me.dionclei.workshopspringboot.services.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -39,6 +42,18 @@ public class UserResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 }
--- src/main/java/me/dionclei/workshopspringboot/services/UserService.java
+++ src/main/java/me/dionclei/workshopspringboot/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import me.dionclei.workshopspringboot.entities.User;
@@ -30,6 +31,23 @@ public class UserService {
 	
 	public User insert(User obj) {
 		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+		
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 	
 }
