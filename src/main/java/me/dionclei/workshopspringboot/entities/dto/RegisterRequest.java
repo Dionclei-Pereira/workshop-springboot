37c1ff5b23807a8eff40e9ccd11acfb6ea773dbe
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2024-12-27 09:51:53 -0300

    Register with spring validation

--- src/main/java/me/dionclei/workshopspringboot/entities/dto/RegisterRequest.java
+++ src/main/java/me/dionclei/workshopspringboot/entities/dto/RegisterRequest.java
@@ -1,7 +1,9 @@
 package me.dionclei.workshopspringboot.entities.dto;
 
-import me.dionclei.workshopspringboot.enums.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record RegisterRequest(String name, String email, String phone, String password) {
+public record RegisterRequest(@NotBlank @Size(min = 3, max = 20, message = "Name's size must be between 3 and 20") String name,@Email @NotBlank String email, String phone,@NotBlank @Size(min = 6, max = 16, message = "Password size must be between 6 and 16") String password) {
 	
 }
